1a8131ae1800d956b52fcd002238e8a94069af05
honchan <honkangchan@gmail.com>
2021-07-02 21:26:55 +0800

    add usersmodule to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './database/database.module';
 import { OrganisationsModule } from './organisations/organisations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), DatabaseModule, OrganisationsModule],
+  imports: [
+    ConfigModule.forRoot({}),
+    DatabaseModule,
+    OrganisationsModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
