a2d55e3a6a8d72e5a92bb9cbc8b245a146d3f27e
honchan <honkangchan@gmail.com>
2021-07-02 22:09:29 +0800

    add featureflag feature

---
+++ src/featureflags/dto/createFeatureFlag.dto.ts
@@ -0,0 +1,5 @@
+export interface CreateFeatureFlagDto {
+  name: string;
+  description: string;
+  organisation: number;
+}
---
+++ src/featureflags/featureflags.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateFeatureFlagDto } from "./dto/createFeatureFlag.dto";
+import { FeatureFlag } from "./featureflag.entity";
+
+@Injectable()
+export class FeatureFlagsService {
+
+  constructor(
+    @InjectRepository(FeatureFlag)
+    private readonly featureFlagsRepository: Repository<FeatureFlag>,
+  ) {}
+
+  async createFeatureFlag(featureFlag: CreateFeatureFlagDto) {
+    const newFeatureFlag = await this.featureFlagsRepository.create(
+      featureFlag,
+    );
+    await this.featureFlagsRepository.save(newFeatureFlag);
+
+    return newFeatureFlag;
+  }
+}
